5149e7cf5333efb30212a4e1e245a6898a60a5d9
Kirill <parahinkv@gmail.com>
2022-04-29 20:48:01 +0300

    Добавьте файлы проекта.

---
+++ See war/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace See_war
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Ivory;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Location = new System.Drawing.Point(163, 43);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(242, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 631);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ See war/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace See_war
+{
+    class coord
+    {
+        public static int number { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+    class coords
+    {
+        public static int range { get; set; }
+        coord[] coordn = new coord[coord.number];
+    }
+    public partial class Form1 : Form
+    {
+        int i,j,j0;
+        bool hod;
+        int[,] field = new int[12, 12];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            hod = true;
+            dataGridView1.Enabled = false;
+            dataGridView1.Visible = false;
+            dataGridView1.ColumnCount = 10;
+            dataGridView1.RowCount = 10;
+            dataGridView1.Width = 510;
+            dataGridView1.Height = 510;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.ColumnHeadersVisible = false;
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                dataGridView1.Rows[i].Height = 50;
+                dataGridView1.Columns[i].Width = 50;
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                    dataGridView1.Rows[i].Cells[j].Style.SelectionBackColor = Color.White;
+            }    
+            dataGridView1.ClearSelection();
+            dataGridView1.ReadOnly = true;
+            dataGridView1.AllowUserToResizeColumns = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            for (int i = 0; i < 12; i++)
+                for (int j = 0; j < 12; j++)
+                    field[i, j] = 0;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var soundPlayer = new SoundPlayer(@"c:\Windows\Media\see_war.wav"))
+            {
+                soundPlayer.Play(); 
+            }
+            dataGridView1.Enabled = true;
+            dataGridView1.Visible = true;
+            button1.Enabled = false;
+            i = 4;
+            j = 1;
+            j0 = j;
+            label1.Text = "Выберите на поле " + i.ToString() +" корабля длины 1";
+
+        }
+
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (hod)
+            {
+                if ((i > 0) && (j0 == j) && field[e.RowIndex + 1, e.ColumnIndex + 1] == 0)
+                {
+                    dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Selected = false;
+                    dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor = Color.Red;
+                    field[e.RowIndex + 1, e.ColumnIndex + 1] = 1;
+                    i -= 1;
+                    label1.Text = "Выберите на поле " + i.ToString() + " корабля длины " + j.ToString();
+                    j0 -= 1;
+                }
+                else if ((i >= 0) && (j0 < j) && (j0 > 0))
+                {
+                    if (field[e.RowIndex + 1, e.ColumnIndex + 1] == 0)
+                    {
+                        if (((field[e.RowIndex, e.ColumnIndex + 1] == 1) && (e.RowIndex > 0)) || ((field[e.RowIndex + 2, e.ColumnIndex + 1] == 1) && (e.RowIndex <= 8)) || ((field[e.RowIndex + 1, e.ColumnIndex + 2] == 1) && (e.ColumnIndex <= 8)) || ((field[e.RowIndex + 1, e.ColumnIndex] == 1) && (e.ColumnIndex > 0)))
+                        {
+                            dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Selected = false;
+                            dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor = Color.Red;
+                            field[e.RowIndex + 1, e.ColumnIndex + 1] = 1;
+                            j0 -= 1;
+                        }
+                    }
+                }
+                if (j < 5)
+                {
+                    if ((i == 0) && (j0 == 0))
+                    {
+                        j += 1;
+                        i = 5 - j;
+                        if ((i > 0) || (j < 5))
+                            label1.Text = "Выберите на поле " + i.ToString() + " корабля длины " + j.ToString();
+                        else label1.Text = "";
+                    }
+                    if (j0 == 0)
+                    {
+                        j0 = j;
+                    }
+                }
+                else
+                {
+                    hod = false;
+                    for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                    {
+                        for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                        {
+                            dataGridView1.Rows[i].Cells[j].Style.BackColor = Color.White;
+                        }
+                    }
+                    label1.Text = "Начинайте угадывание положения вражеских кораблей";
+                }
+            }
+            else
+            {
+                dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Selected = false;
+                if (field[e.RowIndex + 1, e.ColumnIndex + 1] == 1)
+                    dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor = Color.Black;
+                else dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor = Color.Blue;
+            }
+        }
+    }
+}
